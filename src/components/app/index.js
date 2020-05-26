ab3217eb612f70cdd776df1d693a613dbded84d0
Enrique <jmtov4@gmail.com>
2020-05-26 19:21:50 -0300

    Discover - Cleaned up the project.

---
+++ src/components/app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        Movies
+      </header>
+    </div>
+  );
+}
+
+export default App;
